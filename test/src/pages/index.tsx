40c1fd7c5f6b3b9afd38920e0dd947bde69f8a8a
Kseniia Pidopryhora <ksenia.pidopryhora@gmail.com>
2023-07-14 21:30:40 +0300

    auth

--- test/src/pages/index.tsx
+++ test/src/pages/index.tsx
@@ -2,10 +2,11 @@ import Head from "next/head";
 import Link from "next/link";
 import { SignIn } from "@clerk/clerk-react";
 import { api } from "~/utils/api";
-import { SignInButton } from "@clerk/nextjs";
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 
 export default function Home() {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const user = useUser();
 
   return (
     <>
@@ -15,9 +16,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <SignInButton />
-
-        {/* <SignIn /> */}
+        <div>
+          {!user.isSignedIn && <SignInButton />}{" "}
+          {!!user.isSignedIn && <SignOutButton />}
+        </div>
+        <SignIn />
       </main>
     </>
   );
